e03adc6f4a2189a36d264db1c76c2a1a173fa1b0
Vasiliy Tolstov <v.tolstov@selfip.ru>
2016-12-12 14:41:58 +0300

    bump fedora image
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/packer.sh
+++ scripts/packer.sh
@@ -4,6 +4,7 @@ if [ "x$(pwd)" = "x/" ]; then
     cwd="/root"
 fi
 
+curl --output /etc/yum.repos.d/fedora-virt-preview.repo https://fedorapeople.org/groups/virt/virt-preview/fedora-virt-preview.repo
 dnf -y update
 dnf -y install git make patch golang qemu-system-x86 qemu-img
 
@@ -13,10 +14,10 @@ git clone --depth=1 https://github.com/mitchellh/packer.git ${cwd}/tmp/src/githu
 export GOPATH=${cwd}/tmp GOBIN=${cwd}/bin PATH=${PATH}:${cwd}/bin GO15VENDOREXPERIMENT=1
 pushd ${cwd}/tmp/src/github.com/mitchellh/packer
 make deps
-curl -Ls https://github.com/mitchellh/packer/pull/3473.patch | git apply -
-curl -Ls https://github.com/mitchellh/packer/pull/3681.patch | git apply -
-curl -Ls https://github.com/mitchellh/packer/pull/3790.patch | git apply -
+#curl -Ls https://github.com/mitchellh/packer/pull/3473.patch | git apply -
 #curl -Ls https://github.com/mitchellh/packer/pull/3804.patch | git apply -
+#curl -Ls https://github.com/mitchellh/packer/pull/3890.patch | git apply -
+
 popd
 go build -x -tags netgo -v -o /usr/local/bin/packer github.com/mitchellh/packer
 dnf -y remove git-core make patch golang
